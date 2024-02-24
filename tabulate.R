f48614143cce10f7dd182ffbd0abd083b66678a7
FabianiniPrz <usermac@Users-MacBook-Air.local>
2024-02-24 16:57:20 -0500

    I used the factor function and the unique one

--- tabulate.R
+++ tabulate.R
@@ -5,3 +5,11 @@ sum(votes$poll)
 votes$total <- votes$poll + votes$mail
 
 write.csv(votes,"total.csv",row.names = FALSE)
+
+
+URL <- "https://raw.githubusercontent.com/fivethirtyeight/data/master/non-voters/nonvoters_data.csv"
+
+
+voters <- read.csv(URL)
+
+View(voters)
