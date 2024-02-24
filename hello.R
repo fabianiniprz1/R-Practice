01dfa7a3cfb12e33e7c15beea0bb9d5be3c515dd
FabianiniPrz <usermac@Users-MacBook-Air.local>
2024-02-24 06:21:12 -0500

    this is a CS50R practice

---
+++ hello.R
@@ -0,0 +1,8 @@
+# Ask users for name
+name <- readline("What's your name? ")
+
+# Concatenate the name with the phrase Hello, 
+greeting <- paste("Hello, ", name, sep = "")
+
+# it print in console the greeting variable
+print(greeting)
