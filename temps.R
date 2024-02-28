60df8296985606d556c4ab599cb575f8469073fb
FabianiniPrz <usermac@Users-MacBook-Air.local>
2024-02-27 21:23:46 -0500

    I created the temps file and add some calculations

--- temps.R
+++ temps.R
@@ -14,3 +14,28 @@ mean(temps_data$temperature)
 sort(temps_data) 
 
 temps_data$temperature[- c(2,3,7)]
+
+temps_data$temperature[1] > 0
+
+vector_i <- temps_data$temperature[] > 0
+
+temps_data$temperature[vector_i]
+
+
+which(temps_data$temperature[] < 0)
+
+any(temps_data$temperature[] < 0 | temps_data$temperature[] > 60)
+all(temps_data$temperature[] < 0 | temps_data$temperature[] > 60)
+
+temps_data$temperature[] < 0 | temps_data$temperature[] > 60
+
+
+outliers_filter <- temps_data$temperature[] < 0 | temps_data$temperature[] > 60
+
+temps_data$temperature[outliers_filter]
+
+non_outliers_filter <- !(temps_data$temperature[] < 0 | temps_data$temperature[] > 60)
+
+temps_data$temperature[non_outliers_filter]
+
+sort(unique(temps_data$temperature[non_outliers_filter]))
