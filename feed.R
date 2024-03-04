606e06777859ed9b9925fd2f48db733c0a0de343
FabianiniPrz <usermac@Users-MacBook-Air.local>
2024-03-04 07:13:09 -0500

    I created a menu

--- feed.R
+++ feed.R
@@ -5,11 +5,29 @@ chicks <- subset(chicks,!is.na(weight))
 # Determine feed options
 feed_options <- unique(chicks$feed)
 
+# Format options
+num_options <- length(feed_options)
+formated_options <- paste0(1:num_options,'. ',feed_options)
+
 # prompt user with options
-cat("1.  ",feed_options[1],"\n")
-cat("2.  ",feed_options[2],"\n")
-cat("3.  ",feed_options[3],"\n")
-cat("4.  ",feed_options[4],"\n")
-cat("5.  ",feed_options[5],"\n")
-cat("6.  ",feed_options[6],"\n")
-feed_choice <- as.integer(readline("Feed type: "))
+
+# cat(formated_options,sep = "\n")
+# feed_choice <- as.integer(readline("Feed type: "))
+
+# menu option
+
+feed_choice <- menu(
+  feed_options,
+  title = "Feed type"
+)
+
+# Print the selected action
+if(feed_choice < 1 || feed_choice > num_options){
+  cat("Invalid choice")
+} else {
+  selected_feed <- feed_options[feed_choice]
+  print(subset(chicks,feed == selected_feed))  
+}
+
+
+
